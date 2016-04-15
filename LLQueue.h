4327fb330bbc48893877bf797924fa33855a9a43
TomAbrahams <UTKeitarol@gmail.com>
2016-04-15 10:01:52 -0700

    Update LLQueue.h
    Implement the functions in a separate cpp. Solution is provided within the git.

--- LLQueue.h
+++ LLQueue.h
@@ -19,6 +19,15 @@ public:
 	//Enqueue
 	void enqueue(int theData);
 	//Convert
+	//Convert looks at 3 numbers at a time for your doubly linked list.
+	//And converts it to either a 1 or a 0 if all three are a 1 or 0.
+	//For Example:
+	//Input: 1 1 1 0 0 0 1 0 1
+	//Conversion:
+	// 1 1 1 = 1
+	// 0 0 0 = 0
+	// 1 0 1 = Nothing.
+	// Final output should be 1 0
 	void convert();
 	//Print
 	void print() const;
