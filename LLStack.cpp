49d12acccfcf6005d2827fb10ab52c8c0cf70ae1
TomAbrahams <UTKeitarol@gmail.com>
2016-04-15 09:56:37 -0700

    Update LLStack.cpp

--- LLStack.cpp
+++ LLStack.cpp
@@ -52,6 +52,22 @@ void LLStack::push(int theData)
 	}
 
 }
+//Convert looks at 3 numbers at a time for your doubly linked list.
+//And converts it to either a 1 or a 0 if all three are a 1 or 0.
+//For Example:
+//Input: 1 1 1 0 0 0 1 0 1
+//Conversion:
+// 1 1 1 = 1
+//Store as top of stack version of Linked List
+// 0 0 0 = 0
+//Store as top of stack version of Linked List
+// 1 0 1 = Nothing.
+//Stack looks as follows
+// | 0 |
+// | 1 |
+// -----
+// Final output should be 1 0
+// You can make an array in this case.
 void LLStack::convert()
 {
 	int* arrHold = new int[numberOfElements];
